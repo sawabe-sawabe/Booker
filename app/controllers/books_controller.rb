00ea72f82f14b2938bc1dfd52e1e162a52666cac
EC2 Default User <ec2-user@ip-172-31-58-255.ec2.internal>
2021-05-05 07:45:25 +0000

    [Finish]4-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,6 +3,7 @@ class BooksController < ApplicationController
   def index
     @books=Book.all
     @book = Book.new
+
   end
 
   def show
@@ -10,12 +11,13 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
+    @book = Book.new(book_params)
 
-    if book.save
-    redirect_to book_path(book.id),notice:"Book was successfully created."
-
-
+    if @book.save
+    redirect_to book_path(@book.id),notice:"Book was successfully created."
+    else
+    @books=Book.all
+    render 'index'
     end
 
   end
@@ -25,11 +27,14 @@ class BooksController < ApplicationController
  end
 
  def update
-   book = Book.find(params[:id])
+  @book = Book.find(params[:id])
 
-   if book.update(book_params)
+   if @book.update(book_params)
 
-   redirect_to book_path(book.id),notice:"Book was successfully updated."
+   redirect_to book_path(@book.id),notice:"Book was successfully updated."
+   else
+    #@book = Book.find(params[:id])
+    render :edit
 
 
    end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
 resources :books
-get 'top' =>'homes#top'
+root :to =>'homes#top'
 
 end
